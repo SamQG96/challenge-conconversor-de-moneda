b33b9e1f71b8c3daa7bceabe956e7667a7546ec8
Samir Quintero <quinterogarciasamir@gmail.com>
2025-01-18 09:17:53 -0500

    Creación de las clases faltantes

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultaMoneda {
+
+    public Moneda BuscarMoneda() {
+
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/699fac3de1325a009c3f0c24/latest/USD");
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(direccion).build();
+
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Moneda.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Seleccione una opción valida");
+        }
+
+    }
 }
--- src/FiltroMoneda.java
+++ src/FiltroMoneda.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Map;
 
 public class FiltroMoneda {
+    private Moneda moneda;
+
+    public FiltroMoneda(Moneda moneda) {
+        this.moneda = moneda;
+    }
+
+  public double convertirMoneda(String baseCode, double valor) {
+
+      Map<String, Double> conversionRates = moneda.conversion_rates();
+      if (conversionRates.containsKey(baseCode)) {
+          double tasa = conversionRates.get(baseCode);
+          return valor * tasa;
+      }else {
+          throw new IllegalArgumentException("La Moneda de a convertir no valida");
+      }
+
+  }
+
+    public double monedaAnterior (String Code, double valor){
+        Map<String, Double> conversionRates = moneda.conversion_rates();
+        if (conversionRates.containsKey(Code)) {
+            double tasa = conversionRates.get(Code);
+            return valor / tasa;
+        }else {
+            throw new IllegalArgumentException("La Moneda de origen no valida");
+        }
+
+    }
 }
